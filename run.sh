c9e18694df0056156ade748cc531e07b2843a7cf
qiniu <yangjinecho@gmail.com>
2017-12-20 17:27:04 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=./lib ./lib/ld-linux.so ./bin/turnserver -n --no-auth --stun-only --db ./turndb/turndb
+LD_LIBRARY_PATH=./lib exec ./lib/ld-linux.so ./bin/turnserver -n --no-auth --stun-only --db ./turndb/turndb
